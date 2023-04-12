4a5ad4567150e76be35b90f5c494b907d0e72371
tatanach0 <119086180+tatanach0@users.noreply.github.com>
2023-04-12 11:54:31 -0600

    Delete mscv-usuarios directory

--- mscv-usuarios/src/main/java/org/tatanacho/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.tatanacho.springcloud.msvc.usuarios.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.tatanacho.springcloud.msvc.usuarios.models.entity.Usuario;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-}
